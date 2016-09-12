716d3f00635433a9d6bd671fe8a257e4eb948ebb
Joney <1050617313@qq.com>
2016-09-12 23:53:15 +0800

    day 13

---
+++ src/main/java/com/dc/core/spring/reference/FactoryImpl/dao/daoImpl/FactoryImpl.java
@@ -0,0 +1,19 @@
+package com.dc.core.spring.reference.FactoryImpl.dao.daoImpl;
+
+import com.dc.core.spring.reference.FactoryImpl.dao.FactoryDao;
+
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+<<<<<<< HEAD
+
+=======
+//Ctrl+I 实现接口方法
+>>>>>>> 1536c34840cc9d744b3e02696275c07f18f18d09
+public class FactoryImpl implements FactoryDao {
+    @Override
+    public void saveFacoyr() {
+
+    }
+}
+
